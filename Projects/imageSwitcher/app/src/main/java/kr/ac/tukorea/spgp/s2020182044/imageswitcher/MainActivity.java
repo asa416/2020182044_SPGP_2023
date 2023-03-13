1d5a7e5085a9a5bede917e985596071baf94cc89
Hwang Seok Ju <asa4163@gmail.com>
2023-03-13 15:59:27 +0900

    ImageSwitcher 생성

---
+++ Projects/imageSwitcher/app/src/main/java/kr/ac/tukorea/spgp/s2020182044/imageswitcher/MainActivity.java
@@ -0,0 +1,14 @@
+package kr.ac.tukorea.spgp.s2020182044.imageswitcher;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
